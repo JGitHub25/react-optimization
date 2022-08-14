58f9791af934fa79a44e1e5efe950759323f1da1
jpeg25 <jairo25509@hotmail.com>
2022-08-14 10:14:48 -0500

    exercise on React.memo completed

--- src/pages/Exercise3/GraphPoint.js
+++ src/pages/Exercise3/GraphPoint.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 
-export const GraphPoint = () => {
+export const GraphPoint = React.memo(() => {
   const [on] = useState(Math.random() > 0.8);
 
   return <div className={`e3-graph-point ${on ? 'e3-graph-point-on' : ''}`} />;
-};
+})
