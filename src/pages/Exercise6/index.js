d7eed0840b7b146012c2f42057d155741d54e65f
jpeg25 <jairo25509@hotmail.com>
2022-08-14 14:51:27 -0500

    lazy loading component

--- src/pages/Exercise6/index.js
+++ src/pages/Exercise6/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Details from './Details';
-
+// import Details from './Details';
 import './styles.css';
+
+const Details = React.lazy(() => import('./Details'))
+
 
 export const Exercise6 = () => {
   return (
